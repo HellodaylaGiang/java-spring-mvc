c75995ef1689eed451f4c915a14f69a4b0418c6e
Bún Đậu <124412911+HellodaylaGiang@users.noreply.github.com>
2024-03-18 12:54:22 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World from Spring Boot!";
+        return "Hello World from Spring Boot hhhhhhh!";
+    }
+
+    @GetMapping("/user")
+    public String index1() {
+        return "Hello World from Spring Boot User!";
+    }
+
+    @GetMapping("/admin")
+    public String index2() {
+        return "Hello World from Spring Boot admin!";
     }
 }
